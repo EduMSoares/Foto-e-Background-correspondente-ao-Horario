dcf25470dc4ed92f03bc0d9a0fd58217f410d30f
Eduardo Magalhães Soares <90562184+EduMSoares@users.noreply.github.com>
2022-01-04 17:01:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function carregar(){
     msg.innerHTML = `Agora são ${hora} horas`
         if (hora >= 04 && hora < 12){
             //BOMD DIA
-            img.src = '../ex014/manha.png'
+            img.src = 'manha.png'
             document.body.style.background = '#8c7556'
         } else if (hora >=12 && hora <18){
             //BOA TARDE
-            img.src = '../ex014/tarde.png'
+            img.src = 'tarde.png'
             document.body.style.background = '#fccfbc'
         }else {
             //BOA NOITE
-            img.src = '../ex014/noitte.png'
+            img.src = 'noitte.png'
             document.body.style.background = '#9d82b9'
         }
     }
